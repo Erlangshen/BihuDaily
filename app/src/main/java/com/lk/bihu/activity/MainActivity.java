57fb8c92ade12d04158b1e67334e66fbeef331b4
liuergua <841103312@qq.com>
2016-07-11 17:29:04 +0800

    做了一点小工作

--- app/src/main/java/com/lk/bihu/activity/MainActivity.java
+++ app/src/main/java/com/lk/bihu/activity/MainActivity.java
@@ -2,11 +2,13 @@ package com.lk.bihu.activity;
 
 
 import android.view.View;
+import android.widget.Button;
 
 import com.lk.bihu.BihuApplication;
 import com.lk.bihu.R;
 
 public class MainActivity extends BaseActivity {
+    private Button menu;
     @Override
     protected int getLayoutId() {
         return R.layout.activity_main;
@@ -14,22 +16,27 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-
+        menu= (Button) findViewById(R.id.menu);
     }
 
     @Override
     protected void initData() {
-
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showShortToast("此处应该有个目录弹出");
+            }
+        });
     }
 
-    public void change(View view) {
-        BihuApplication app= (BihuApplication) getApplication();
-        app.setNightMode(!app.isNightMode());
-        if (app.isNightMode()){
-            ChangeToNight();
-        }else{
-            ChangeToDay();
-        }
-        recreate();
-    }
+//    public void change(View view) {
+//        BihuApplication app= (BihuApplication) getApplication();
+//        app.setNightMode(!app.isNightMode());
+//        if (app.isNightMode()){
+//            ChangeToNight();
+//        }else{
+//            ChangeToDay();
+//        }
+//        recreate();
+//    }
 }
---
+++ app/src/main/java/com/lk/bihu/fragment/ContentFragment.java
@@ -0,0 +1,20 @@
+package com.lk.bihu.fragment;
+
+import android.view.View;
+
+public class ContentFragment extends BaseFragment{
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void initView(View v) {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
